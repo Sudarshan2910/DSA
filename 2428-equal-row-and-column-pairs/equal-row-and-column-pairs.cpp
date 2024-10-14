01249c814603d3fa95629bdd3c89f3f9e5841704
Sudarshan Goyal <144123788+Sudarshan2910@users.noreply.github.com>
2024-10-14 05:52:16 +0530

    Time: 53 ms (95.78%) | Memory: 30.4 MB (76.56%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        vector<vector<int>> b= grid;
+        transpose(grid,b);
+        int count=0;
+        map<vector<int>,int> m;
+        for(int i=0;i<grid[0].size();i++)
+        {
+            m[grid[i]]++;
+        }
+        for(int i=0;i<grid[0].size();i++)
+        {
+            if(m.find(b[i])!=m.end())
+            {
+                count+=m[b[i]];
+            }
+        }
+        return count;
+    
+    }
+    void transpose(vector<vector<int>>& a,vector<vector<int>>& b)
+    {
+        int i,j;
+        for(i=0;i<a[0].size();i++)
+        {
+            for(j=0;j<a[0].size();j++)
+            {
+                b[i][j]=a[j][i];
+            }
+        }
+    }
+    // bool compare(vector<int>& v1,vector<int>& v2)
+    // {
+    //     for(int i=0;i<v1.size();i++)
+    //     {
+    //         if(v1[i]!=v2[i])return false;
+    //     }
+    //     return true;
+    // }
+};
