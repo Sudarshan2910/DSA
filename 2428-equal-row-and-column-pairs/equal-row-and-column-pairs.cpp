faae46a1fc99d6c903e8f6392f59ec6011868116
Sudarshan Goyal <144123788+Sudarshan2910@users.noreply.github.com>
2024-10-14 05:52:54 +0530

    Time: 59 ms (89.02%) | Memory: 30.2 MB (76.92%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -30,12 +30,4 @@ public:
             }
         }
     }
-    // bool compare(vector<int>& v1,vector<int>& v2)
-    // {
-    //     for(int i=0;i<v1.size();i++)
-    //     {
-    //         if(v1[i]!=v2[i])return false;
-    //     }
-    //     return true;
-    // }
 };
