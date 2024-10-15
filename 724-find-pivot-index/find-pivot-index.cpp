3f5bf84fda6e0349e7be5ffe70668adaaff04a39
Sudarshan Goyal <144123788+Sudarshan2910@users.noreply.github.com>
2024-10-15 23:16:00 +0530

    Time: 19 ms (44.65%) | Memory: 36 MB (15.48%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {       
+        vector<int> start(nums.size(),0);
+        vector<int> end(nums.size(),0);
+        int sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(i==0)
-            {
-                int sl=0;
-                int sr=summation(nums,1,nums.size()-1);
-                if(sl==sr)return 0;
-            }
-            else if(i==nums.size()-1)
-            {
-                int sr=0;
-                int sl=summation(nums,0,nums.size()-2);
-                if(sl==sr)return nums.size()-1;
-            }
-            else
-            {
-                int sl=summation(nums,0,i-1);
-                int sr=summation(nums,i+1,nums.size()-1);
-                if(sl==sr)return i;
-            }
+            sum+=nums[i];
+            start[i]=sum;
+        }
+        sum=0;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            sum+=nums[i];
+            end[i]=sum;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(start[i]==end[i])return i;
         }
         return -1;
-    }
-    int summation(vector<int>& n,int s,int e)
-    {
-        int sum=0;
-        for(int i=s;i<=e;i++)
-        {
-            sum+=n[i];
-        }
-        return sum;
     }
 };
